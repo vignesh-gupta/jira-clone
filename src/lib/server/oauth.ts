c3ff7e487d518003f1f78015979fb3abbabc3c5b
Vignesh <vighneshgupta32@gmail.com>
2024-10-25 12:45:35 +0530

    revert: rollback oauth prod changes

--- src/lib/server/oauth.ts
+++ src/lib/server/oauth.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { redirect } from "next/navigation";
+import { headers } from "next/headers";
 import { OAuthProvider } from "node-appwrite";
 
 import { createAdminClient } from "@/lib/appwrite";
@@ -8,10 +9,12 @@ import { createAdminClient } from "@/lib/appwrite";
 export async function signUpWithGithub() {
   const { account } = await createAdminClient();
 
+  const origin = headers().get("origin");
+
   const redirectUrl = await account.createOAuth2Token(
     OAuthProvider.Github,
-    `${process.env.NEXT_PUBLIC_APP_URL}/oauth`,
-    `${process.env.NEXT_PUBLIC_APP_URL}/sign-up`
+    `${origin}/oauth`,
+    `${origin}/sign-up`
   );
 
   return redirect(redirectUrl);
