30c498525cb82fcd5501f7f4407696161da4d119
Vignesh <vighneshgupta32@gmail.com>
2024-10-25 12:12:25 +0530

    fix: oauth issue for prod

--- src/lib/server/oauth.ts
+++ src/lib/server/oauth.ts
@@ -1,19 +1,17 @@
 "use server";
 
-import { createAdminClient } from "@/lib/appwrite";
 import { redirect } from "next/navigation";
-import { headers } from "next/headers";
 import { OAuthProvider } from "node-appwrite";
+
+import { createAdminClient } from "@/lib/appwrite";
 
 export async function signUpWithGithub() {
   const { account } = await createAdminClient();
 
-  const origin = headers().get("origin");
-
   const redirectUrl = await account.createOAuth2Token(
     OAuthProvider.Github,
-    `${origin}/oauth`,
-    `${origin}/sign-up`
+    `${process.env.NEXT_PUBLIC_APP_URL}/oauth`,
+    `${process.env.NEXT_PUBLIC_APP_URL}/sign-up`
   );
 
   return redirect(redirectUrl);
